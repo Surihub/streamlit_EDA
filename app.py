8227013585626b240610de2475d0b9e176f72bc1
surish <sbhath17@gmail.com>
2023-10-30 04:21:55 +0900

    first commit

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("Exploratory Data Analysis")
 st.write("Hello, statistics!")
+
+name = st.text_input("이름을 입력해주세요.")
+if name != "":
+    st.write(f"반가워요, {name}님!")
+    st.write("# 😃")
